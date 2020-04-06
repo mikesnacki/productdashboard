b84d6fb902ce4c799083caa37185a8e6d51ceb91
mikesnacki <mikesnacki@gmail.com>
2020-04-06 07:02:17 -0400

    header changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,18 +8,18 @@ import DropdownMenu from "./DropdownMenu"
 export default function Header(){
     const size = useWindowSize()
     const width = size.width
-    const collapseWidth = 960
+    const collapseWidth = 900
     const scrollDirection = useScrollDirection()
     const [navDisplay, activateNavDisplay] = useState(false)
     const { isAuthenticated, loginWithRedirect, logout} = useAuth0();
       
     const links = [
-      <Link key={1} className="header-text nav-links" to="/">Home</Link>,
-      isAuthenticated && <Link key={2} className="header-text nav-links flex-row-no-wrap" to="/projects">Projects</Link>,
-      isAuthenticated && <Link key={3} className="header-text nav-links flex-row-no-wrap" to="/addproject">Add a Project</Link>,
-      isAuthenticated && <Link key={4} className="header-text nav-links flex-row-no-wrap" to="/profile">Profile</Link>,
-      !isAuthenticated && <span key={5} className="header-text nav-links flex-row-no-wrap" onClick={() => loginWithRedirect({})}>Login</span>,
-      isAuthenticated && <span key={6} className="header-text nav-links flex-row-no-wrap" onClick={() => logout()}>Logout</span>,
+      <Link key={1} className="s" to="/">Home</Link>,
+      isAuthenticated && <Link key={2} className="s" to="/projects">Projects</Link>,
+      isAuthenticated && <Link key={3} className="s" to="/addproject">Add a Project</Link>,
+      isAuthenticated && <Link key={4} className="s" to="/profile">Profile</Link>,
+      !isAuthenticated && <span key={5} className="s" onClick={() => loginWithRedirect({})}>Login</span>,
+      isAuthenticated && <span key={6} className="s" onClick={() => logout()}>Logout</span>,
     ]
 
     return(    
@@ -30,18 +30,16 @@ export default function Header(){
             <h1 className="header-text">User Stories</h1>
             {
               width >= collapseWidth ? 
-              <ul className="nav-links">{links}</ul>
+              <ul className="">{links}</ul>
               :
               <button
                 className={`menu-bar menu-button`}
                 onClick={() => activateNavDisplay(!navDisplay)}
               >
-                <div className='flex-row'>
-                  <div className="flex-col">
+                <div>
                     <div className="line1"></div>
                     <div className="line2"></div>
                     <div className="line3"></div>
-                  </div>
                 </div> 
               <DropdownMenu
                 links={links}
