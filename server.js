a18db7253391b7c39e9627efb2b656f627f68382
mikesnacki <mikesnacki@gmail.com>
2020-04-08 16:51:19 -0400

    to restrict cors, or not?

--- server.js
+++ server.js
@@ -16,24 +16,7 @@ mongoose.connect(db,{
         }).then(()=>console.log("Mongo connected"))
         .catch(err=>console.log(err));
 
-const allowedOrigins = [
-                      'http://localhost:3000',
-                      'https://localhost:3000',
-                      'http://storydashboard.herokuapp.com',
-                      'httpS://storydashboard.herokuapp.com'
-                      ]
-
-app.use(cors({
-        origin: function(origin, callback){
-          if(!origin) return callback(null, true);
-          if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                      'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-          }
-          return callback(null, true);
-        }
-      }));
+app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
