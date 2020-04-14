9bf9246d8db9265689fb696b3f03ae217510c13b
mikesnacki <mikesnacki@gmail.com>
2020-04-14 14:08:25 -0400

    static serve

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(
+    expressStaticGzip(path.join(__dirname, 'build'), {
+    }))
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
