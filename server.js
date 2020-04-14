a63b1e58aa2ca8a8b877e9996a13961475225142
mikesnacki <mikesnacki@gmail.com>
2020-04-14 14:17:23 -0400

    take 2

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require("path");
 const app = express();
 const routes = require("./server/routes/routes.js");
 const jwtCheck = require("./server/auth/auth")
+var expressStaticGzip = require('express-static-gzip');
 
 mongoose.connect(db,{
             useNewUrlParser: true,
@@ -27,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(
     expressStaticGzip(path.join(__dirname, 'build'), {
     }))
-    app.use(express.static('client/build'));
+    // app.use(express.static('client/build'));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
